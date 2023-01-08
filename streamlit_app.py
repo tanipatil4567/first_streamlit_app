379b28093de43cd11b78a38dbc00b94d48e2ac51
tanipatil4567 <63333719+tanipatil4567@users.noreply.github.com>
2023-01-08 11:37:10 +0530

    Update streamlit_app.py
    display the table of response

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+#arange the data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display table on the page
+streamlit.dataframe(fruityvice_normalized)
    
